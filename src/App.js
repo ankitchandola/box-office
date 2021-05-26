e651252905a3c690e90fceffb7603391e22f9cce
ankit <sindeadly25@gmail.com>
2021-05-26 18:11:43 +0530

    mainpage and nav adjust

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
